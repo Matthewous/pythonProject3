f1c98c555ed5a281f39212f2fcba54b14c023b08
Matthewous <moguch98@mail.ru>
2022-09-18 18:31:34 +0300

    Файлы перенесены в папку

---
+++ py-homeworks-advanced/Regular_expressions/test.py
@@ -0,0 +1,16 @@
+# import re
+
+# pattern = "(\+7|8)?\s*\(?(\d\d\d)\D?\s?(\d\d\d)\D?\s?(\d\d)\D?\s?(\d\d)(\s)?\(?([д][о][б]\.)?[\s]?(\d+)?"
+# substitution = r"+7(\2)\3-\4-\5\6\7\8"
+
+# text = 
+
+
+# result = re.sub(pattern, substitution, text)
+# pattern = "(\+7|8)?\s*\(?(\d\d\d)\D?\s?(\d\d\d)\D?\s?(\d\d)\D?\s?(\d\d)(\s)?\(?([д][о][б]\.)?[\s]?(\d+)?"
+# substitution = r"+7(\2)\3-\4-\5\6\7\8"
+# result = re.sub(pattern, substitution, text)
+
+import os
+
+print(os.listdir())
