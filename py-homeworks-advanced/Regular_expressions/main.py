439f17f63dd08237499fd673b67624c86a881573
Matthewous <moguch98@mail.ru>
2022-09-20 00:00:24 +0300

    Перед обновлением

--- py-homeworks-advanced/Regular_expressions/main.py
+++ py-homeworks-advanced/Regular_expressions/main.py
@@ -2,31 +2,34 @@ from pprint import pprint
 import csv
 import re
 
+def file_opener(filepath):
+
+    with open(filepath) as f:
+        rows = csv.reader(f, delimiter=",")
+        contacts_list = list(rows)
+
+def phones_correction(contacts_list):
+
+    # привести все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
+
+    pattern = r"(\+7|8)?\s*\(?(\d\d\d)\D?\s?(\d\d\d)\D?\s?(\d\d)\D?\s?(\d\d)(\s)?\(?([д][о][б]\.)?[\s]?(\d+)?\)?"
+    substitution = r"+7(\2)\3-\4-\5\6\7\8"
+
+    for line in contacts_list:
+        result = re.sub(pattern, substitution, line[5])
+        line[5] = result
+
+
 filepath = "/Users/matthew/Desktop/pythonProject3/py-homeworks-advanced/Regular_expressions/phonebook_raw.csv"
 
-with open(filepath) as f:
-    rows = csv.reader(f, delimiter=",")
-    contacts_list = list(rows)
-# pprint(contacts_list)
+
+if __name__ == '__main__':
+
+    contacts_list = file_opener(filepath)
 
 # TODO поместить Фамилию, Имя и Отчество человека в поля lastname, firstname и surname соответственно. В записной книжке изначально может быть Ф + ИО, ФИО, а может быть сразу правильно: Ф+И+О
 
-
-# привести все телефоны в формат +7(999)999-99-99. Если есть добавочный номер, формат будет такой: +7(999)999-99-99 доб.9999;
-pattern = r"(\+7|8)?\s*\(?(\d\d\d)\D?\s?(\d\d\d)\D?\s?(\d\d)\D?\s?(\d\d)(\s)?\(?([д][о][б]\.)?[\s]?(\d+)?\)?"
-substitution = r"+7(\2)\3-\4-\5\6\7\8"
-
-for line in contacts_list:
-    counter = 1
-    for object in line:
-        if counter == 6:
-            result = re.sub(pattern, substitution, object)
-            line[counter-1] = result
-            break
-        else: counter += 1
-    # pprint(line)
-# pprint(contacts_list)
-
+    phones_correction(contacts_list)
 
 # TODO объединить все дублирующиеся записи о человеке в одну.
 
--- py-homeworks-advanced/Regular_expressions/test.py
+++ py-homeworks-advanced/Regular_expressions/test.py
@@ -1,16 +1,47 @@
-# import re
+import csv
+from pprint import pprint
+import re
 
-# pattern = "(\+7|8)?\s*\(?(\d\d\d)\D?\s?(\d\d\d)\D?\s?(\d\d)\D?\s?(\d\d)(\s)?\(?([д][о][б]\.)?[\s]?(\d+)?"
-# substitution = r"+7(\2)\3-\4-\5\6\7\8"
+filepath = "/Users/matthew/Desktop/pythonProject3/py-homeworks-advanced/Regular_expressions/phonebook_raw.csv"
 
-# text = 
+with open(filepath) as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
+
+# pprint(contacts_list)
+
+# print(contacts_list[1])
+# def last_name(contacts_list):
+#     # pattern = r"^((\w+)\s?(\w+)?\s?(\w+)?\,)"
+#     pattern = r"^(\w+)[\s|\,](\w+)[\s|\,](\w+)?(\,\,?\,?)"
+
+#     substitution_lastname = r"\1"
+#     substitution_firstname = r"\2"
+#     substitution_surname = r"\3"
+
+#     for line in contacts_list:
+#         # pprint(line[0])
+#         lastname = re.sub(pattern, substitution_lastname, line[0])
+#         firstname = re.sub(pattern, substitution_firstname, line[0])
+#         surname = re.sub(pattern, substitution_surname, line[0])
+
+#         new = []
+#         new.append(lastname)
+#         pprint(new)
+#     return contacts_list
+# last_name(contacts_list)
 
 
-# result = re.sub(pattern, substitution, text)
-# pattern = "(\+7|8)?\s*\(?(\d\d\d)\D?\s?(\d\d\d)\D?\s?(\d\d)\D?\s?(\d\d)(\s)?\(?([д][о][б]\.)?[\s]?(\d+)?"
-# substitution = r"+7(\2)\3-\4-\5\6\7\8"
-# result = re.sub(pattern, substitution, text)
+# ^(\w+)[\s|\,](\w+)[\s|\,](\w+)?(\,\,?\,?)
+# pprint(contacts_list)
 
-import os
+pattern = r"^(\w+)[\s*|\,](\w+)[\s*|\,](\w+)?(\,\,?\,?)"
+substitution = r"\1,\2,\3,"
 
-print(os.listdir())
+
+for line in contacts_list:
+    c = str(line[0])
+    result = re.sub(pattern, substitution, c)
+    pprint(result)
+# pprint(contacts_list)
+
