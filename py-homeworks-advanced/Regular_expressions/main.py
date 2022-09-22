1b05bf889ee9bcc2f3d71f967ddc183922f10f4e
Matthewous <moguch98@mail.ru>
2022-09-22 23:52:18 +0300

    Убраны дубли

--- py-homeworks-advanced/Regular_expressions/main.py
+++ py-homeworks-advanced/Regular_expressions/main.py
@@ -57,8 +57,61 @@ if __name__ == '__main__':
     name_correction(contacts_list)
     phones_correction(contacts_list)
 
+    print(contacts_list)
+
 # TODO объединить все дублирующиеся записи о человеке в одну.
 
+new_list = []
+compared = []
+
+num = 0
+num_compare = 0
+for line in contacts_list:
+    if line[0] not in compared:
+        lastname = line[0]
+        firstname = line[1]
+        num += 1
+        # print(lastname)
+        for line_compare in contacts_list:
+            num_compare += 1
+            lastname_compare = line_compare[0]
+            firstname_compare = line_compare[1]
+            if num != num_compare and lastname == lastname_compare and firstname == firstname_compare:
+                new_list_line = {}
+                
+                new_list_line[0] = lastname
+                new_list_line[1] = firstname
+                
+                keys = range(7)
+                # print(num_compare)
+                # print(line)
+                # print(line_compare)
+
+                for if_num in keys:
+                    # print(new_list_line)
+                    # print(line_compare)
+
+                    if line[if_num] == "":
+                        new_list_line[if_num] = line_compare[if_num]
+                        # pprint(line_compare)
+                    else:
+                        new_list_line[if_num] = line[if_num]
+                    
+                    # print(new_list_line[if_num])
+
+                # code = 0 
+                # for new_line in new_list:
+                #     for lastnames in new_line.values():
+                #         if new_list_line[0] == lastnames:
+                #             code = 1
+
+                compared.append(new_list_line[0])
+                new_list.append(new_list_line)
+
+                # if code == 0:
+                #     new_list.append(new_list_line)
+
+# pprint(new_list)
 
 # TODO 2: сохраните получившиеся данные в другой файл
 # код для записи файла в формате CSV
