334c719ad3d0e2e88dd2dcf333cfc96b2c01d0e8
Matthewous <moguch98@mail.ru>
2022-11-20 19:42:16 +0300

    Flat_generator

---
+++ py-homeworks-advanced/Iterators_and_generators/flat_generator.py
@@ -0,0 +1,39 @@
+# Должен получиться генератор, который принимает список списков и возвращает их плоское представление. 
+
+import types
+
+list_of_lists_1 = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None]
+]
+
+def flat_generator(list_of_lists):
+    for list_num in range(0,len(list_of_lists)):
+        for number in range(0,len(list_of_lists[list_num])):
+            yield list_of_lists[list_num][number]
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_2()
+    print(list(flat_generator(list_of_lists_1)))
+    
