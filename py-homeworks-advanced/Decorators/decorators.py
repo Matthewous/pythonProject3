5cbe84890a0d2526575fbee295fad5558c514228
Matthewous <moguch98@mail.ru>
2023-01-12 23:19:15 +0300

    Успешное прохождение текста

--- py-homeworks-advanced/Decorators/decorators.py
+++ py-homeworks-advanced/Decorators/decorators.py
@@ -6,7 +6,7 @@ def logger(old_function):
         call_time = datetime.now().strftime('%d-%m-%Y время %H:%M:%S')
         old_func_name = old_function.__name__
         res = old_function(*args, **kwargs)
-        with open('main.log', 'a', encoding='utf-8') as file:
+        with open('/Users/matthew/Desktop/pythonProject3/py-homeworks-advanced/Decorators/logs/main.log', 'a', encoding='utf-8') as file:
             file.write(f'Время вызова функции - {call_time}, '
                        f'имя функции - {old_func_name}, '
                        f'аргументы функции - {args, kwargs}, '
